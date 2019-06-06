294a5e22f645fc8e2f3936a445354fac69ceba75
cywisp <cywisp@gmail.com>
2019-06-06 12:37:11 -0400

    update

--- modules/pygame/basics/crash_course_exercises/practice_2/settings.py
+++ modules/pygame/basics/crash_course_exercises/practice_2/settings.py
@@ -4,9 +4,16 @@ class Settings():
     def __init__(self):
         # Initialize all of the game's settings
         # Screen settings
-        self.screen_width = 800
-        self.screen_height = 600
+        self.screen_width = 1200    
+        self.screen_height = 800
         self.bg_color = (0, 0, 0)
 
         # Ship settings
-        self.ship_speed_factor = 1.5
+        self.ship_speed_factor = 1.25
+
+        # Bullet settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (230, 230, 0)
+        
