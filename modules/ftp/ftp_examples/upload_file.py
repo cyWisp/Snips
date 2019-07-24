29fb7ea2faa26628717502a5c1f079c3467caf29
cywisp <cywisp@gmail.com>
2019-07-24 11:11:42 -0700

    updated

---
+++ modules/ftp/ftp_examples/upload_file.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from ftplib import FTP
+
+def place_file(ftp_object):
+
+    file_name = 'test.txt'
+    ftp_object.storbinary('STOR' + file_name, open(file_name, 'rb'))
+
+if __name__ == '__main__':
+
+    # define url, creds, establish connection and log in
+    creds = ('cybersherpa.net', 'cywisp', 'DEFCONwasFUN1985$')
+    ftp = FTP(creds[0])
+    ftp.login(user=creds[1], passwd=creds[2])
+
+    # change current working directory before upload
+    # and show contents
+    ftp.cwd('/public_html/')
+    #ftp.dir()
+    
+    # upload 'test.txt'
+    place_file(ftp)
+
+    # show contents to prove file has been uploaded
+    ftp.dir()
+    ftp.quit()
+
+
+    
+
+
+
+
+
