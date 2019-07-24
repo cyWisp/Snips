b1b4d9b70d08e61193a4210622c442fa4cf1d635
cywisp <cywisp@gmail.com>
2019-07-24 11:20:59 -0700

    updated

--- modules/ftp/ftp_examples/upload_file.py
+++ modules/ftp/ftp_examples/upload_file.py
@@ -1,34 +1,11 @@
 #!/usr/bin/env python
 from ftplib import FTP
 
-def place_file(ftp_object):
-
-    file_name = 'test.txt'
-    ftp_object.storbinary('STOR' + file_name, open(file_name, 'rb'))
-
 if __name__ == '__main__':
 
-    # define url, creds, establish connection and log in
-    creds = ('domain', 'username', 'password')
-    ftp = FTP(creds[0])
-    ftp.login(user=creds[1], passwd=creds[2])
+    session = FTP('domain', 'username', 'password')
+    upload_file = open('test.txt', 'rb')
 
-    # change current working directory before upload
-    # and show contents
-    ftp.cwd('/public_html/')
-    #ftp.dir()
-    
-    # upload 'test.txt'
-    place_file(ftp)
-
-    # show contents to prove file has been uploaded
-    ftp.dir()
-    ftp.quit()
-
-
-    
-
-
-
-
-
+    session.storbinary('STOR test.txt', upload_file)
+    upload_file.close()
+    session.quit()
